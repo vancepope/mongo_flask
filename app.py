6bf9228abdad2321597e9c726063dd86e9a236ae
vancepope <vancepope@outlook.com>
2023-01-25 15:55:52 -0800

    Updated .gitignore, added routes to app.py, updated requirements.txt, added tests in test_app.py

--- app.py
+++ app.py
@@ -15,20 +15,49 @@ app = Flask(__name__)
 uri = os.getenv("MONGO_DATABASE_URL")
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
-client = MongoClient(f'mongodb+srv://{username}:{password}@popecluster.vdqwgwk.mongodb.net/?retryWrites=true&w=majority')
 
-db = client['crud']
-collection = db['addColl']
-doc_count = collection.count_documents({})
-print(doc_count)
+client = MongoClient(f'mongodb+srv://{username}:{password}@cluster0.1fqhb55.mongodb.net/?retryWrites=true&w=majority')
 
-@app.route("/")
+db = client['store']
+collection = db['parts']
+
+@app.get("/")
 def index():
     data = []
     for item in collection.find():
         data.append(item)
     return json.dumps(data, indent=4, default=json_util.default), 200
 
+@app.get("/part/<part_id>")
+def get_part(part_id):
+    part = collection.find_one({"_id": ObjectId(part_id)})
+    if part:
+        return json_util.dumps(part), 200
+    else:
+        return json_util.dumps({"error": "Item not found."}), 404
 
+@app.post("/part")
+def create_part():
+    part = request.get_json()
+    result = collection.insert_one(part)
+    return json_util.dumps({"_id": str(result.inserted_id)})
+    
+@app.put("/part/<part_id>")
+def update_part(part_id):
+    item = request.get_json()
+    result = collection.update_one({"_id": ObjectId(part_id)}, {"$set": item})
+    if result.matched_count > 0:
+        return json_util.dumps({"status": "Success"})
+    else: 
+        return json_util.dumps({"error": "Item not found"}), 404
+    
+@app.delete("/delete_part/<part_id>")
+def delete_part(part_id):
+    result = collection.delete_one({"_id": ObjectId(part_id)})
+    if result.deleted_count > 0:
+        return json_util.dumps({"status": "Success"}), 200
+    else: 
+        return json_util.dumps({"error": "Item not found"}), 404
+    
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ test_app.py
@@ -0,0 +1,122 @@
+from dotenv import load_dotenv
+from bson.objectid import ObjectId
+import datetime as date
+from unittest.mock import Mock, patch
+
+objectid = ObjectId()
+@patch("pymongo.collection.Collection.find")
+def find(mocker):
+    mocker.return_value = [{"_id": {"$oid": objectid},
+                                    "part_name": "Screw", 
+                                    "part_number": "4R5E3", 
+                                    "quantity": 100, 
+                                    "backorder": False, 
+                                    "manufacturer": "Screws N More", 
+                                    "weight": .01, 
+                                    "dimensions": "16mm", 
+                                    "cost": .10, 
+                                    "expiration": "12/24/25"
+                            }]
+    mocker.returncode = 200
+    return mocker;
+
+@patch("pymongo.collection.Collection.find_one")
+def find_one(mocker):
+    mocker.return_value = [{"_id": {"$oid": objectid}, 
+                                    "part_name": "Screw", 
+                                    "part_number": "4R5E3", 
+                                    "quantity": 100, 
+                                    "backorder": False, 
+                                    "manufacturer": "Screws N More", 
+                                    "weight": .01, 
+                                    "dimensions": "16mm", 
+                                    "cost": .10, 
+                                    "expiration": "12/24/25"
+                            }]
+    mocker.returncode = 200
+    return mocker;
+
+@patch("pymongo.collection.Collection.find_one")
+def find_one_fail(mocker):
+    mocker.return_value = [{"error": "Item not found"}]
+    mocker.returncode = 404
+    return mocker;
+
+@patch("pymongo.collection.Collection.insert_one")
+def insert_one(mocker):
+    mocker.return_value = [{"_id": objectid}]
+    mocker.returncode = 200
+    return mocker;
+
+@patch("pymongo.collection.Collection.update_one")
+def update_one(mocker):
+    mocker.return_value = [{"status": "Success"}]
+    mocker.returncode = 200
+    return mocker;
+
+@patch("pymongo.collection.Collection.update_one")
+def update_one_fail(mocker):
+    mocker.return_value = [{"error": "Item not found"}]
+    mocker.returncode = 404
+    return mocker;
+
+@patch("pymongo.collection.Collection.delete_one")
+def delete_one(mocker):
+    mocker.return_value = [{"status": "Success"}]
+    mocker.returncode = 200
+    return mocker;
+
+@patch("pymongo.collection.Collection.delete_one")
+def delete_one_fail(mocker):
+    mocker.return_value = [{"error": "Item not found"}]
+    mocker.returncode = 404
+    return mocker;
+
+def test_index():
+    data = find()
+    assert len(data.return_value) > 0
+    assert data.returncode == 200
+    print(f"Expected Output: length == 1 | Output: length == {len(data.return_value)}")
+    print(f"Expected Output: returncode == 200 | Output: returncode == {data.returncode}")
+
+def test_get_part():
+    data = find_one()
+    assert len(data.return_value) > 0
+    assert data.returncode == 200
+    print(f'Expected Output: length > 0 | Output: {len(data.return_value)} > 0')
+    print(f"Expected Output: returncode == 200 | Output: returncode == {data.returncode}")
+
+def test_get_part_fail():
+    data = find_one_fail()
+    assert data.return_value[0]["error"] == "Item not found"
+    assert data.returncode == 404
+    print(f'Expected Output: status == Item not found | Output: status == {data.return_value[0]["error"]}')
+    print(f"Expected Output: returncode == 404 | Output: returncode == {data.returncode}")
+    
+def test_update_part():
+    data = update_one()
+    assert data.return_value[0]["status"] == "Success"
+    assert data.returncode == 200
+    print(f'Expected Output: status == Success | Output: status == {data.return_value[0]["status"]}')
+    print(f"Expected Output: returncode == 200 | Output: returncode == {data.returncode}")
+
+def test_update_part_fail():
+    data = update_one_fail()
+    assert data.return_value[0]["error"] == "Item not found"
+    assert data.returncode == 404
+    print(f'Expected Output: status == Item not found | Output: status == {data.return_value[0]["error"]}')
+    print(f"Expected Output: returncode == 404 | Output: returncode == {data.returncode}")
+
+def test_delete_part():
+    data = delete_one()
+    assert data.return_value[0]["status"] == "Success"
+    assert data.returncode == 200
+    print(f'Expected Output: status == Success | Output: status == {data.return_value[0]["status"]}')
+    print(f"Expected Output: returncode == 200 | Output: returncode == {data.returncode}")
+
+def test_delete_part_fail():
+    data = delete_one_fail()
+    assert data.return_value[0]["error"] == "Item not found"
+    assert data.returncode == 404
+    print(f'Expected Output: status == Item not found | Output: status == {data.return_value[0]["error"]}')
+    print(f"Expected Output: returncode == 404 | Output: returncode == {data.returncode}")
